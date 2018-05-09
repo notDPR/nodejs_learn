aca8ae91491be16c6b9da1693b1ec5b7edbe80ef
Himanshu Santolia <himanshusantolia@live.com>
2018-05-09 23:34:18 +0530

    Add projects page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about' , (req,res)=>{
 	}) ;
 });
 
+app.get('/projects' , (req,res)=>{
+	res.render('projects.hbs' , {
+		pageTitle : 'Projects Page'
+	}) ;
+});
+
+
 
 app.listen(port , ()=>{
 	console.log("Server is up on port :" , port) ;
